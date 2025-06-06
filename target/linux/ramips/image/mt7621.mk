fee403bb9105214547bc86760df09770bff1fa6b
sgccc1 <sgccc1@163.com>
2025-06-06 09:28:08 +0800

    e8820s

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -3468,6 +3468,24 @@ define Device/zio_freezio
 	kmod-usb-ledtrig-usbport -uboot-envtools
 endef
 TARGET_DEVICES += zio_freezio
+define Device/zte_e8820s
+  $(Device/uimage-lzma-loader)
+  $(Device/dsa-migration)
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 4096k
+  UBINIZE_OPTS := -E 5
+  IMAGE_SIZE := 129280k
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
+	check-size
+  DEVICE_VENDOR := ZTE
+  DEVICE_MODEL := E8820S
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 \
+	kmod-usb3
+endef
+TARGET_DEVICES += zte_e8820s
 
 define Device/zyxel_lte3301-plus
   $(Device/nand)
